f83fd69098cfbd096f3285f867845c24ed128fbd
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-11-24 23:08:51 +0900

    포스트 상세 페이지 대강 구현

--- js/post-detail.js
+++ js/post-detail.js
@@ -1,79 +1,83 @@
-async function getPost(postId){
-	let writerId;
-	try{
-		const res = await axios.get(server+`/api/posts/${postId}`)
-		post = res.data.data
-		$("#post-title").text(post.title)
-		$("#post-date").text(post.createdAt.slice(0,10))
-		$("#post-body").html(post.content)
-		$("#post-like-count").text(post.likes)
-		$("#post-like-btn").click(async function() {
-			await axios.put(server+`/api/posts/${postId}/like`)
-			
-		})
-		writerId = post.userId
-	}catch(e){
-		console.log(e)
-		return alert("존재하지 않는 포스트입니다.")
-	}
-	try{
-		const writer = await axios.get(server+`/api/user/${writerId}`)
-		$("#post-writer").text(writer.data.data.name)
-	}catch(e){
-		$("#post-writer").text("탈퇴한 사용자")
-	}
+async function getPost(postId) {
+    let writerId;
+    try {
+        const res = await axios.get(server + `/api/posts/${postId}`);
+        post = res.data.data;
+        $("#post-title").text(post.title);
+        $("#post-date").text(post.createdAt.slice(0, 10));
+        $("#post-body").html(post.content);
+        $("#post-like-count").text(post.likes);
+        $("#post-like-btn").click(async function () {
+            await axios.put(server + `/api/posts/${postId}/like`);
+        });
+        writerId = post.userId;
+    } catch (e) {
+        console.log(e);
+        return alert("존재하지 않는 포스트입니다.");
+    }
+    try {
+        const writer = await axios.get(server + `/api/user/${writerId}`);
+        $("#post-writer").text(writer.data.data.name);
+    } catch (e) {
+        $("#post-writer").text("탈퇴한 사용자");
+    }
 }
 
-async function getComments(postId){
-	let comments = []
-	try{
-		const res = await axios.get(server+`/api/comments/?postId=${postId}`)
-		comments = res.data.postComments
-	}catch(e){
-		console.log(e)
-	}
-	for(let comment of comments)
-		try{
-			const writer = await axios.get(server+`/api/user/${comment.userId}`)
-			const commentBar = $(`<div class="comment-bar"></div>`)
-			commentBar.append(`<p class="comment-writer">${writer.data.data.name}</p>
-			<p class="comment-date">${comment.createdAt.slice(0,10)}</p>`)
-			const commentLikeBtn = $(`<button class="comment-like-btn comment-btn">&#x1F44D;</button>`)
-			commentLikeBtn.click(async function() {
-				await axios.put("/api/comments/${comment.id}/like")
-			})
-			commentBar.append(commentLikeBtn)
-			const editBtn = $(`<button class="edit-btn comment-btn">&#x270D;</button>`)
-			editBtn.click(async function() {
-				$(".comment-btn").each(function() {
-					$(this).attr("disabled","true")
-				})
-				const newSubmit = $(`<button>수정</button>`)
-				const commentBody = commentHTML.find(".comment-body")
-				commentBody.attr("contenteditable", "true")
-				commentHTML.append(newSubmit)
-				newSubmit.click(async function(){
-					await axios.put("/api/comments/${comment.id}", {content:"commentBody.text()"})
-					newSubmit.remove()
-					commentBody.attr("contenteditable", "false")
-					$(".comment-btn").each(function() {
-						$(this).removeAttr("disabled")
-					})
-				})	
-			})
-			commentBar.append(editBtn)
-			const deleteBtn = $(`<button class="delete-btn comment-btn">&#10006;</button>`)
-			deleteBtn.click(async function() {
-				const deleteVal = confirm("댓글을 삭제하겠습니까?")
-				if(deleteVal)
-					await axios.delete("/api/comments/${comment.id}")
-			})
-			commentBar.append(deleteBtn)
-			const commentHTML = $(`<div class="comment"></div>`)
-			commentHTML.append(commentBar)
-			commentHTML.append(`<blockquote contenteditable="false" class="comment-body">${comment.content}</blockquote>`)
-			$("#comments").append(commentHTML)
-		}catch(e){
-			console.log(e)
-		}
+async function getComments(postId) {
+    let comments = [];
+    try {
+        const res = await axios.get(server + `/api/comments/?postId=${postId}`);
+        comments = res.data.postComments;
+    } catch (e) {
+        console.log(e);
+    }
+    for (let comment of comments)
+        try {
+            const writer = await axios.get(server + `/api/user/${comment.userId}`);
+            const commentBar = $(`<div class="comment-bar"></div>`);
+            commentBar.append(`<p class="comment-writer">${writer.data.data.name}</p>
+			<p class="comment-date">${comment.createdAt.slice(0, 10)}</p>`);
+            const commentLikeBtn = $(
+                `<button class="comment-like-btn comment-btn">&#x1F44D;</button>`
+            );
+            commentLikeBtn.click(async function () {
+                await axios.put("/api/comments/${comment.id}/like");
+            });
+            commentBar.append(commentLikeBtn);
+            const editBtn = $(`<button class="edit-btn comment-btn">&#x270D;</button>`);
+            editBtn.click(async function () {
+                $(".comment-btn").each(function () {
+                    $(this).attr("disabled", "true");
+                });
+                const newSubmit = $(`<button>수정</button>`);
+                const commentBody = commentHTML.find(".comment-body");
+                commentBody.attr("contenteditable", "true");
+                commentHTML.append(newSubmit);
+                newSubmit.click(async function () {
+                    await axios.put("/api/comments/${comment.id}", {
+                        content: "commentBody.text()",
+                    });
+                    newSubmit.remove();
+                    commentBody.attr("contenteditable", "false");
+                    $(".comment-btn").each(function () {
+                        $(this).removeAttr("disabled");
+                    });
+                });
+            });
+            commentBar.append(editBtn);
+            const deleteBtn = $(`<button class="delete-btn comment-btn">&#10006;</button>`);
+            deleteBtn.click(async function () {
+                const deleteVal = confirm("댓글을 삭제하겠습니까?");
+                if (deleteVal) await axios.delete("/api/comments/${comment.id}");
+            });
+            commentBar.append(deleteBtn);
+            const commentHTML = $(`<div class="comment"></div>`);
+            commentHTML.append(commentBar);
+            commentHTML.append(
+                `<blockquote contenteditable="false" class="comment-body">${comment.content}</blockquote>`
+            );
+            $("#comments").append(commentHTML);
+        } catch (e) {
+            console.log(e);
+        }
 }
