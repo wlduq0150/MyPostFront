6ec54f38214b1a8b55f95024bf83fdb33d1eed6d
wlduq0150 <wlduq0150@gmail.com>
2023-11-27 12:32:29 +0900

    유저 상세 페이지 팔로우 표현 추가

--- js/user-detail.js
+++ js/user-detail.js
@@ -1,9 +1,28 @@
-function uploadProfile() {
+async function uploadProfile() {
+    const response = await axios.get(server + `/api/user/${user.id}/followList`, {
+        headers
+    });
+
+    const followList = response.data.followList;
+
     const name = document.getElementById('name');
     const email = document.getElementById('email');
     const birth =  document.getElementById('birthdate');
+    const followingList = document.getElementById("followingList");
 
     name.value = user.name;
     email.value = user.email;
     birth.value = user.birth.slice(0, 10);
+
+    for (let fl of followList) {
+        const res = await axios.get(server+`/api/user/${fl.followeeId}`);
+        const user = res.data.data;
+
+        console.log(user);
+
+        const li = document.createElement("li");
+        li.textContent = user.name;
+
+        followingList.appendChild(li);
+    }
 }
